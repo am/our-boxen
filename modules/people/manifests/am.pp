4eadb5833da971e1c32aad45eacab0d136049161
Antonio Miranda <antsmiranda@gmail.com>
2013-10-13 12:48:23 -0700

    Add 10.9 compatible params

--- modules/people/manifests/am.pp
+++ modules/people/manifests/am.pp
@@ -3,8 +3,7 @@ class people::am {
   include people::am::params
   include people::am::applications
   include people::am::repositories
-  # https://github.com/glarizza/puppet-property_list_key/issues/12
-  # include people::am::config
+  include people::am::config
   include people::am::vm
   include people::am::projects
 
--- modules/people/manifests/am/config.pp
+++ modules/people/manifests/am/config.pp
@@ -9,55 +9,54 @@ class people::am::config (
   ###################
 
   # NOTE: Dock prefs only take effect when you restart the dock
-  property_list_key { 'Hide the dock':
-    ensure     => present,
-    path       => "${my_homedir}/Library/Preferences/com.apple.dock.plist",
-    key        => 'autohide',
-    value      => true,
-    value_type => 'boolean',
-    notify     => Exec['Restart the Dock'],
-  }
+  #property_list_key { 'Hide the dock':
+  #  ensure     => present,
+  #  path       => "${my_homedir}/Library/Preferences/com.apple.dock.plist",
+  #  key        => 'autohide',
+  #  value      => true,
+  #  value_type => 'boolean',
+  #  notify     => Exec['Restart the Dock'],
+  #}
 
   exec { 'Restart the Dock':
     command     => '/usr/bin/killall -HUP Dock',
     refreshonly => true,
   }
 
-  property_list_key { 'Lower Left Hotcorner - Screen Saver':
-    ensure     => present,
-    path       => "${my_homedir}/Library/Preferences/com.apple.dock.plist",
-    key        => 'wvous-bl-corner',
-    value      => 10, 
-    value_type => 'integer',
-    notify     => Exec['Restart the Dock'],
-  }
+  #property_list_key { 'Lower Left Hotcorner - Screen Saver':
+  #  ensure     => present,
+  #  path       => "${my_homedir}/Library/Preferences/com.apple.dock.plist",
+  #  key        => 'wvous-bl-corner',
+  #  value      => 10, 
+  #  value_type => 'integer',
+  #  notify     => Exec['Restart the Dock'],
+  #}
 
-  property_list_key { 'Lower Left Hotcorner - Screen Saver - modifier':
-    ensure     => present,
-    path       => "${my_homedir}/Library/Preferences/com.apple.dock.plist",
-    key        => 'wvous-bl-modifier',
-    value      => 0,
-    value_type => 'integer',
-    notify     => Exec['Restart the Dock'],
-  }
+  #property_list_key { 'Lower Left Hotcorner - Screen Saver - modifier':
+  #  ensure     => present,
+  #  path       => "${my_homedir}/Library/Preferences/com.apple.dock.plist",
+  #  key        => 'wvous-bl-modifier',
+  #  value      => 0,
+  #  value_type => 'integer',
+  #  notify     => Exec['Restart the Dock'],
+  #}
 
-  file { 'Dock Plist':
-    ensure  => file,
-    require => [
-                 Property_list_key['Hide the dock'],
-                 Property_list_key['Lower Left Hotcorner - Screen Saver'],
-                 Property_list_key['Lower Left Hotcorner - Screen Saver - modifier'],
-               ],  
-    path    => "${my_homedir}/Library/Preferences/com.apple.dock.plist",
-    mode    => '0600',
-    notify     => Exec['Restart the Dock'],
-  }
+  #file { 'Dock Plist':
+  #  ensure  => file,
+  #  require => [
+  #               Property_list_key['Hide the dock'],
+  #               Property_list_key['Lower Left Hotcorner - Screen Saver'],
+  #               Property_list_key['Lower Left Hotcorner - Screen Saver - modifier'],
+  #             ],  
+  #  path    => "${my_homedir}/Library/Preferences/com.apple.dock.plist",
+  #  mode    => '0600',
+  #  notify  => Exec['Restart the Dock'],
+  #}
 
-  property_list_key { 'Disable Gatekeeper':
-    ensure => present,
-    path   => '/var/db/SystemPolicy-prefs.plist',
-    key    => 'enabled',
-    value  => 'no',
+  exec { "Disable Gatekeeper":
+    command => "spctl --master-disable",
+    path    => "/usr/sbin/",
+    user => root,
   }
 
   exec { "set_computer_name":
